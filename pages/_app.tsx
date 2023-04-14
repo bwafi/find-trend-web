fc76716b505b22c685bd7bda87683749c1efec52
Syahroni Bawafi <syahronibwf@gmail.com>
2023-04-14 16:46:31 +0700

    initial commit and navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const effra = localFont({
+  src: [
+    {
+      path: '../public/fonts/Effra.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Effra-Heavy.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-effra',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${effra.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
