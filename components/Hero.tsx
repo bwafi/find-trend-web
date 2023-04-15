17ff4fdbf8d3dac2574df8649335a9e3df98cb4a
Syahroni Bawafi <syahronibwf@gmail.com>
2023-04-16 00:55:31 +0700

    card hero and responsive section hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,22 +3,45 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <div className="w-full flex bg-cover h-screen bg-hero text-white flex-col items-center gap-7">
-      <div className="flex mt-52 flex-col">
-        <p className="text-center text-7xl font-efrra font-extrabold">Minimize your tabs.</p>
-        <p className="text-center text-7xl font-efrra font-extrabold">Find the trends!</p>
+    <div className="w-full relative flex bg-cover h-auto bg-hero text-white flex-col items-center gap-7">
+      <div className="flex lg:mt-52 mt-32 flex-col gap-3">
+        <p className="text-center lg:text-7xl md:text-5xl text-4xl font-efrra font-extrabold">Minimize your tabs.</p>
+        <p className="text-center lg:text-7xl md:text-5xl text-4xl font-efrra font-extrabold">Find the trends!</p>
       </div>
-      <div className="w-6/12 flex justify-center">
-        <p className="text-center font-efrra text-lg">
+      <div className="lg:w-6/12 md:w-6/12 w-10/12 flex justify-center">
+        <p className="text-center font-efrra lg:text-lg md:text-base text-sm">
           Don’t let your computer memories consumes all of those browser tabs. Findtrend let you gathers all of your
           favorite website into one place.
         </p>
       </div>
-      <div className="flex justify-end items-center w-4/12">
-        <button className="bg-green px-8 py-4 font-efrra text-lg font-bold rounded-full text-black">
+      <div className="flex justify-center md:ml-32 ml-0 gap-4 items-center w-10/12">
+        <button className="bg-green px-8 py-4 font-efrra lg:text-lg text-xs font-bold rounded-full text-black">
           Get Started 🔥
         </button>
-        <Image src="/img/arrow-img-hero.png" alt="hero-img" width={100} height={70} />
+        <Image src="/img/arrow-img-hero.png" className="hidden md:block" alt="hero-img" width={100} height={70} />
+      </div>
+
+      <div className="lg:w-8/12 md:w-7/12 relative lg:flex-nowrap lg:justify-start justify-center flex h-auto lg:mt-24 mt-12 pb-52 flex-wrap w-full">
+        <div className="bg-[#181818] static z-[4] -rotate-[4deg] flex items-center gap-2 h-16 shadow-card-sosmed px-14 lg:absolute card-gradient">
+          <Image src="/icon/twitter.svg" alt="twitter" width={32} height={32} />
+          <p>Cryptopunk - Search</p>
+          <Image src="/icon/cancel.svg" alt="cancel" className="z-10 absolute right-3" width={15} height={15} />
+        </div>
+        <div className="bg-[#181818] rotate-2 top-11 left-40 flex items-center gap-2 py-4 z-[3] shadow-card-sosmed px-10 static lg:absolute card-gradient">
+          <Image src="/icon/pinterest.svg" alt="pinterest" width={32} height={32} />
+          <p>Popular Design - Search</p>
+          <Image src="/icon/cancel.svg" alt="cancel" className="z-10 absolute right-3" width={15} height={15} />
+        </div>
+        <div className="bg-[#181818] -rotate-2 z-[2] right-48 flex items-center gap-2 py-4 shadow-card-sosmed px-10 static lg:absolute card-gradient">
+          <Image src="/icon/facebook.svg" alt="facebook" width={30} height={30} />
+          <p>Product Design - Search</p>
+          <Image src="/icon/cancel.svg" alt="cancel" className="z-10 absolute right-3" width={15} height={15} />
+        </div>
+        <div className="bg-[#181818] rotate-[4deg] top-10 right-0 flex items-center gap-2 py-4 shadow-card-sosmed px-14 static lg:absolute card-gradient">
+          <Image src="/icon/basket.svg" alt="basket" width={32} height={32} />
+          <p>Elon Musk - Search</p>
+          <Image src="/icon/cancel.svg" alt="cancel" className="z-10 absolute right-3" width={15} height={15} />
+        </div>
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 export default function Navbar() {
   const [menu, setMenu] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  useEffect(() => {
+    const navbarBackground = () => {
+      if (window.scrollY > 1) {
+        setNavbar(true);
+      } else {
+        setNavbar(false);
+      }
+    };
+    window.addEventListener('scroll', navbarBackground);
+
+    return () => {
+      window.removeEventListener('scroll', navbarBackground);
+    };
+  }, []);
 
   return (
-    <nav className="flex w-full lg:py-8 py-6 text-white items-center justify-between left-0 top-0 fixed">
-      <div className="flex lg:ml-3 ml-0 cursor-pointer justify-center items-center lg:w-2/12 w-1/4">
+    <nav
+      className={`flex w-full z-50 md:py-6 py-4 text-white items-center justify-between left-0 top-0 fixed ${
+        navbar ? 'backdrop-blur' : ''
+      }`}>
+      <div className="flex ml-3 cursor-pointer lg:justify-center items-center lg:w-2/12 w-1/4">
         <Image src="./icon/find-trend.svg" alt="Fint Trend" width={40} height={40} />
         <h1 className="font-efrra font-bold text-2xl ml-2 hidden md:block">Findtrend</h1>
       </div>
-      <button onClick={() => setMenu(!menu)} className="flex flex-col gap-4 absolute right-5 lg:hidden">
+      <button
+        onClick={() => setMenu(!menu)}
+        className="flex flex-col gap-4 absolute right-5 lg:hidden w-12 h-10 items-center justify-center">
         <span
           className={`w-6 h-[2px] bg-white origin-top-left ${
             menu ? 'rotate-45 translate-x-[1px]' : ''
@@ -26,11 +47,11 @@ export default function Navbar() {
           menu ? '' : '-translate-x-full'
         } `}>
         <ul className="flex font-effra items-center flex-wrap lg:flex-nowrap lg:space-x-7 space-x-0 lg:w-7/12 lg:flex-row flex-col justify-center lg:space-y-0 space-y-9">
-          <li className="text-lg text-center cursor-pointer">About</li>
-          <li className="text-lg text-center cursor-pointer">How it work</li>
-          <li className="text-lg text-center cursor-pointer">Pricing</li>
-          <li className="text-lg text-center cursor-pointer">Solution </li>
-          <li className="text-lg text-center cursor-pointer">Features </li>
+          <li className="text-lg text-center cursor-pointer nav-list relative">About</li>
+          <li className="text-lg text-center cursor-pointer nav-list relative">How it work</li>
+          <li className="text-lg text-center cursor-pointer nav-list relative">Pricing</li>
+          <li className="text-lg text-center cursor-pointer nav-list relative">Solution </li>
+          <li className="text-lg text-center cursor-pointer nav-list relative">Features </li>
         </ul>
 
         <div className="flex space-y-8 lg:space-y-0 items-center font-efrra lg:space-x-7 lg:w-3/12 flex-wrap lg:flex-nowrap justify-center flex-col lg:flex-row">
